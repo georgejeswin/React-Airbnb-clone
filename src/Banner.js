cdae23091f97d27aeed5c2fbe24bd1ad1c329d57
georgejeswin <jeswinmyladoor@gmail.com>
2020-11-16 16:21:08 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,37 @@
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import "./Banner.css";
+import Search from "./Search";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { useHistory } from "react-router-dom";
+
+function Banner() {
+  const history = useHistory();
+  const [showSearch, setShowSearch] = useState(false);
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          variant="outlined"
+          className="banner__searchbutton"
+        >
+          {showSearch ? "Hide" : "Search Dates"}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and strech your imagination</h1>
+        <h5>
+          Plan a different type of getaway to uncover the hidden gems near you
+        </h5>
+        <Button onClick={() => history.push("/search")} variant="outlined">
+          Explore nearby
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
